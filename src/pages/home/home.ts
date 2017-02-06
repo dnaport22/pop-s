4ab2b1414f8e9bbca4d2141da83794bb1d395276
deep-nav <navdeep.dhuti@cameronandwilding.com>
2017-02-06 12:17:46 +0000

    Fetching data from events brite and rendering in card view.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/map';
 
 import { NavController } from 'ionic-angular';
 
@@ -7,9 +10,31 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  events = null;
+  events_name = null;
+  events_description = null;
+  events_image = null;
+  events_img_url = null;
+  mydata = [];
+  constructor(public http: Http) {
+    const endpoint = 'https://www.eventbriteapi.com/v3/events/search/?token=4GD7NTDMEHS6K7GSVVLJ&q=lsbu';
+      this.http.get(endpoint).map(res => res.json()).subscribe(data => {
+          this.events = data.events;
 
-  constructor(public navCtrl: NavController) {
-    
+          for (var i=0; i < this.events.length; i++) {
+              if (this.events[i].logo === null) {
+                  this.mydata.push({
+                      name: this.events[i].name.html,
+                      img: 'no-image',
+                  })
+              } else {
+                  this.mydata.push({
+                      name: this.events[i].name.html,
+                      img: this.events[i].logo.url,
+                  })
+              }
+          }
+      });
   }
 
 }
